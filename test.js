416f5a679962befb8a137632b6dcbe6e55148a7a
mirkobechini <mirkobechini@gmail.com>
2026-06-01 22:25:35 +0200

    update CI workflow to use Node.js versions 18.x and 20.x; fix test precision check

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import {conversionEuroYen} from './index.js';
 
 const yen = conversionEuroYen(1);
 
-if(yen === 185.74) {
+if(yen.toFixed(2) === '185.74') {
     console.log('✅ Test passed');
     process.exit(0);
 } else {
